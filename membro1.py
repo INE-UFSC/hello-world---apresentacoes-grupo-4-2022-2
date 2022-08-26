5c62198312fc7e6a49a213c9cc8963fa5a2932ff
Augusto <augusto.h.v.guerner@gmail.com>
2022-08-26 11:33:05 -0300

    Apresentação membro 1 e 2

--- membro1.py
+++ membro1.py
@@ -1,2 +1,15 @@
 def hello():
-	return "Olá, eu sou o membro1" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+
+    informacoes = """Informações - Olá, eu sou Augusto, tenho 19 anos e atualmente estou estudando.\n"""
+
+    hobbies = """Hobbies - Gosto de ler, escrever e praticar esporte\n"""
+
+    experiencias_previas = """Experiência - Ja fiz estágio na área de desenvolvimento e prático por conta. Tenho conhecimentos de C++, Python, Java, javascript, html, css.\n"""
+
+    expectativa_da_disciplina = """Expectativa - Espero construir bases sólidas em POO\n"""
+
+
+	return informacoes + hobbies + experiencias_previas + expectativa_da_disciplina
+
+
+#escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
--- membro2.py
+++ membro2.py
@@ -1,2 +1,7 @@
 def hello():
-	return "Olá, eu sou o membro2" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return """Olá, eu sou o Micael Presotto e tenho 18 anos. 
+Desde que tive contato com a programação eu gostei muito da área e decidi seguir. 
+Dos meus hobbies, eu gosto de praticar esportes e assistir.
+Minhas experiências foram no ensino médio onde tive contato com python,html,css, JS,mobile e banco de dados.
+Da disciplina espero que me ajude a aumentar meus conhecimentos em orientação à objetos e que com isso ajude a aumentar as minhas bases na área de programação.
+""" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
