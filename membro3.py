bcf9f22e2744c132b9b38db4c3b8cac9ee844282
joaopedrotheodoro <joaopedrth@gmail.com>
2022-08-26 14:27:29 -0300

    Apresentação João Theodoro

--- membro3.py
+++ membro3.py
@@ -1,2 +1,7 @@
 def hello():
-	return "Olá, eu sou o membro3" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return """Olá, eu sou o João Pedro Theodoro e tenho 19 anos. 
+	Sei tocar piano e gosto bastante de ler. 
+	Já fiz um curso tecnico em desenvolvimento de sistemas, 
+	tenho algum conhecimento em python, C#, html, css e javascript.
+	Da disciplina espero aumentar conhecimentos em orientação à objetos e minhas bases na área de programação"""
+	#escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
