97ca07729b7ce52c0487804e5f8a93258e8f73a8
Gian <105215197+Giancarloff@users.noreply.github.com>
2022-08-29 08:30:36 -0300

    Apresentação Gian Carlo

---
+++ membro4.py
@@ -0,0 +1,11 @@
+def hello(_ee = False):
+
+    _str = "Gian Carlo, 24, gosto de jogar no pc, sou técnico em informátia e espero passar com nota máxima sem grandes dificuldades.";
+
+    if (_ee):
+        _str + " (https://www.youtube.com/watch?v=dQw4w9WgXcQ)";
+
+    return _str;
+
+
+#escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
