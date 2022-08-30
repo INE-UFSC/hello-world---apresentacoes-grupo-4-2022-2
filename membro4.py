dfba2f0c08269832b35b66923c43a60997e69af0
Gian <105215197+Giancarloff@users.noreply.github.com>
2022-08-30 09:38:28 -0300

    add newline

--- membro4.py
+++ membro4.py
@@ -1,6 +1,6 @@
 def hello(_ee = False):
 
-    _str = "Gian Carlo, 24, gosto de jogar no pc, sou técnico em informátia e espero passar com nota máxima sem grandes dificuldades.";
+    _str = "\nGian Carlo, 24, gosto de jogar no pc, sou técnico em informátia e espero passar com nota máxima sem grandes dificuldades.";
 
     if (_ee):
         _str = _str + " (https://www.youtube.com/watch?v=dQw4w9WgXcQ)";
