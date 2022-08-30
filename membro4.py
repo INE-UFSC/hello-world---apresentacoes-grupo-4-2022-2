9ce767d712ca4325acf8cd9a55496f4192527950
Gian <105215197+Giancarloff@users.noreply.github.com>
2022-08-30 09:32:08 -0300

    corrigi problema gravíssimo para o caso _ee = True

--- membro4.py
+++ membro4.py
@@ -3,7 +3,7 @@ def hello(_ee = False):
     _str = "Gian Carlo, 24, gosto de jogar no pc, sou técnico em informátia e espero passar com nota máxima sem grandes dificuldades.";
 
     if (_ee):
-        _str + " (https://www.youtube.com/watch?v=dQw4w9WgXcQ)";
+        _str = _str + " (https://www.youtube.com/watch?v=dQw4w9WgXcQ)";
 
     return _str;
 
